Fixed data normalization and added custom loss

LSTM/experiment.py
@@ -8,7 +8,8 @@ import torch.nn as nn
 import numpy as np
 import matplotlib.pyplot as plt
 
-from model.VolatilityLSTM import VolatilityLSTM
+from model.volatility_lstm import VolatilityLSTM
+from model.loss import VolatilityLoss
 from utils.gather_data import read_data
 from utils.dataset_factory import create_dataloaders
 from utils import file_utils
@@ -29,8 +30,8 @@ class Experiment:
 
         # create DataLoaders
         self.data = read_data(**self.config_data['dataset'])
-        self.train_loader, self.val_loader, self.test_loader = create_dataloaders(self.data,
-                                                                                  **self.config_data['loaders'])
+        loaders, self.mean, self.std = create_dataloaders(self.data, **self.config_data['loaders'])
+        self.train_loader, self.val_loader, self.test_loader = loaders
 
         # get batch sizes
         self.n_train_batches = len(self.train_loader)
@@ -48,7 +49,7 @@ class Experiment:
         # initialize model and optimizers
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.model = VolatilityLSTM(**self.config_data['model'])
-        self.criterion = nn.MSELoss(reduction='mean')
+        self.criterion = nn.MSELoss()
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.config_data['experiment']['learning_rate'])
         self.scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(self.optimizer, self.n_epochs)
 
@@ -74,6 +75,7 @@ class Experiment:
     def init_model(self):
         if self.device == 'cuda':
             self.model = self.model.cuda().float()
+            self.model.dense_cuda()
             self.criterion = self.criterion.cuda()
 
     def run(self):
@@ -104,6 +106,7 @@ class Experiment:
     def train(self, epoch: int) -> float:
         self.model.train()
         training_loss = 0
+        total_targets = None
 
         for i, (rolling_window, targets) in enumerate(self.train_loader):
             rolling_window = rolling_window.to(device=self.device)
@@ -217,12 +220,13 @@ class Experiment:
         plt.legend(loc='best')
         plt.title(self.name + ' Stats Plot')
         plt.savefig(os.path.join(self.save_dir, 'stat_plot.png'))
+        plt.close()
 
     def plot_predictions(self, targets: torch.Tensor, preds_force: torch.Tensor, preds_infer: torch.Tensor):
         days = np.arange(targets.size(1))
-        target_arr = (targets[0, :].cpu().detach().numpy() * 0.5472) + 1.5966
-        pred_force_arr = (preds_force[0, :].cpu().detach().numpy() * 0.5472) + 1.5966
-        pred_infer_arr = (preds_infer[0, :].cpu().detach().numpy() * 0.5472) + 1.5966
+        target_arr = (targets[0, :].cpu().detach().numpy() * self.std) + self.mean
+        pred_force_arr = (preds_force[0, :].cpu().detach().numpy() * self.std) + self.mean
+        pred_infer_arr = (preds_infer[0, :].cpu().detach().numpy() * self.std) + self.mean
 
         fig, ax = plt.subplots(3, 1)
         ax[0].plot(days, target_arr)
@@ -236,4 +240,4 @@ class Experiment:
     def print_stats(self, stat_type: str, epoch: int, iteration: int, loss: float):
         print(f'{stat_type.capitalize()}:\t'
               f'[{epoch}/{self.n_epochs}][{iteration}/{getattr(self, f"n_{stat_type}_batches")}]\t'
-              f'Average Loss: {loss / (iteration + 1):.4e}')
+              f'Average Loss: {loss / (iteration + 1):.4f}')

LSTM/model/volatility_lstm.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 from typing import List, Tuple
-from LSTM.model.tensor_funcs import tensor_window_slide
+from model.tensor_funcs import tensor_window_slide
 
 
 class VolatilityLSTM(nn.Module):
@@ -90,7 +90,7 @@ class VolatilityLSTM(nn.Module):
         init_preds, hiddens = self.forward(X)
         final_pred = init_preds[:, -1]
 
-        new_preds = torch.zeros(X.size(0), n_days)
+        new_preds = torch.zeros(X.size(0), n_days).to(device=X.device)
         new_preds[:, 0] = final_pred
 
         # slides the window one day to the right
@@ -105,3 +105,7 @@ class VolatilityLSTM(nn.Module):
             return torch.cat((init_preds, new_preds), dim=1)
         else:
             return new_preds
+
+    def dense_cuda(self):
+        for layer in self.dense_layers:
+            layer = layer.to(device='cuda')

LSTM/utils/dataset_factory.py
@@ -1,22 +1,25 @@
 from torch.utils.data import Dataset, DataLoader
 import torch
 from pandas import DataFrame
-from typing import Tuple, Any
+from typing import Tuple
+import numpy as np
 
 
 class TickerDataset(Dataset):
-    def __init__(self, ticker_df: DataFrame, window_size: int):
+    def __init__(self, ticker_df: DataFrame, window_size: int, mean: float, std: float):
         super().__init__()
         self.tickers = ticker_df['Adj Close'].columns.values
         self.ticker_data = ticker_df
         self.window_size = window_size
+        self.mean = mean
+        self.std = std
 
     def __getitem__(self, index: int) -> Tuple[torch.Tensor, torch.Tensor]:
         """returns input and target tensors for one ticker symbol"""
         daily_close = self.ticker_data['Adj Close']
 
         log_returns = torch.tensor(daily_close[self.tickers[index]], dtype=torch.float).log10().diff()
-        log_returns = (log_returns - 1.5966) / 0.5472
+        log_returns = (log_returns - self.mean) / self.std
         input = log_returns.unfold(0, self.window_size, 1)  # turns 1d tensor into 2d of shape
                                                             # (n_days - window_size, window_size)
         target = log_returns[self.window_size:]  # 1d tensor of shape (n_days - window_size)
@@ -33,7 +36,7 @@ def create_dataloaders(ticker_df: DataFrame,
                        shuffle: bool,
                        n_workers: int,
                        val_ratio: float,
-                       test_ratio: float) -> Tuple[DataLoader, DataLoader, DataLoader]:
+                       test_ratio: float) -> Tuple[Tuple[DataLoader, DataLoader, DataLoader], float, float]:
     """
     :param ticker_df: DataFrame for tickers
     :param window_size: number of days in rolling window
@@ -45,16 +48,17 @@ def create_dataloaders(ticker_df: DataFrame,
     :return: three data loaders (train, val, test)
     """
     dataframes = train_val_test_split(ticker_df, val_ratio, test_ratio)
+    mean, std = find_mean_std(dataframes[0])
     shuffles = (shuffle, False, False)
     loaders = []
 
     for df, should_shuffle in zip(dataframes, shuffles):
-        loaders.append(DataLoader(TickerDataset(df, window_size),
+        loaders.append(DataLoader(TickerDataset(df, window_size, mean, std),
                                   batch_size=batch_size,
-                                  shuffle=should_shuffle,
+                                  shuffle=True,
                                   num_workers=n_workers))
 
-    return tuple(loaders)
+    return tuple(loaders), mean, std
 
 
 def train_val_test_split(data: DataFrame,
@@ -69,3 +73,17 @@ def train_val_test_split(data: DataFrame,
     test_data = data[-n_days_test:].copy()
 
     return train_data, val_data, test_data
+
+
+def find_mean_std(data: DataFrame) -> Tuple[float, float]:
+    log_closes = data['Adj Close'].apply(lambda x: np.log10(x)).diff().dropna()
+    mean = log_closes.mean().mean()
+    std = log_closes.values.std(ddof=1)
+    return mean, std
+
+
+def find_min_max(data: DataFrame) -> Tuple[float, float]:
+    log_closes = data['Adj Close'].apply(lambda x: np.log10(x)).diff().dropna()
+    min = log_closes.min().min()
+    max = log_closes.max().max()
+    return min, max